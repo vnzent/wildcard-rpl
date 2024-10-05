f50a8739c012b5034b562572a28ce93e04549cb4
Adi Aryasuta <adiryasuta.dev@gmail.com>
2024-10-05 14:26:13 +0800

    feat: add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,19 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->createMany([
+            ['name' => 'Arikusuma Wardana', 'email' => 'arikusuma@arijaya.com'],
+            ['name' => 'Nauval Khilmi', 'email' => 'nauvalkhilmi@arijaya.com'],
+            ['name' => 'Adi Aryasuta', 'email' => 'adiaryasuta@arijaya.com'],
+            ['name' => 'Vincent Dua Orang', 'email' => 'vincentptk@arijaya.com'],
+            ['name' => 'Momet Dwika', 'email' => 'mometdwika@arijaya.com'],
+        ]);
     }
 }
