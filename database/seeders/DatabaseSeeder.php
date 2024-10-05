4eafbf3bd781de3768bd7cc22caf92e657b1325f
Adi Aryasuta <adiryasuta.dev@gmail.com>
2024-10-05 14:26:13 +0800

    feat: add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,13 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call(UserSeeder::class);
