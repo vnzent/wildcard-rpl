98696b9e520047146ea18f2788fd0962aa32df8f
Adi Aryasuta <adiryasuta.dev@gmail.com>
2024-10-06 22:30:33 +0800

    feat: add category and product model

---
+++ database/migrations/2024_10_06_141732_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Category::class)->nullable()->constrained()->cascadeOnUpdate()->nullOnDelete();
+            $table->string('code', 20)->unique();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->unsignedInteger('quantity')->default(0);
+            $table->unsignedInteger('price');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
