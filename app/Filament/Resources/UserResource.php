db513722fdc729ed674b6d84302a506a871bb6e9
= <=>
2024-10-18 18:43:48 +0800

    feat: add user filament resource

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\Role;
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use Exception;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Validation\Rules\Password;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+
+    protected static ?string $navigationGroup = 'Administrator';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Fieldset::make('Personal Information')
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required(),
+                        Forms\Components\TextInput::make('email')
+                            ->email()
+                            ->required()
+                            ->unique(),
+                    ]),
+                Forms\Components\Fieldset::make('Authorization')
+                    ->schema([
+                        Forms\Components\Select::make('role')
+                            ->options(Role::class)
+                            ->required()
+                            ->placeholder('Select a role'),
+                    ]),
+                Forms\Components\Fieldset::make('Password')
+                    ->schema([
+                        Forms\Components\TextInput::make('password')
+                            ->required()
+                            ->password()
+                            ->maxLength(255)
+                            ->rule(Password::default()),
+                    ]),
+            ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('role')
+                    ->badge()
+                    ->color(fn (Role $state): string => match ($state) {
+                        Role::ADMINISTRATOR => 'warning',
+                        Role::INVENTORY_MANAGER => 'success',
+                        Role::CASHIER => 'danger',
+                    })
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->since()
+                    ->dateTimeTooltip()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->defaultSort('created_at', 'desc');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
